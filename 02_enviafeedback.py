bdd65dba3dad72a010ea7baa88a1993dec5fc078
rafazamp <rafaelprimieri@hotmail.com>
2020-05-12 16:01:29 -0300

    Create 02_enviafeedback.py

---
+++ 02_enviafeedback.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import os
+import requests
+
+path = "/data/feedback/"
+url = "http://35.224.103.147/feedback/"
+
+
+for file in os.listdir(path):
+    tipos = ["title","name","date","feedback"]
+    dados = {}
+    linhas = []
+    print(file)
+    with open(dir+"/"+file,"r") as txtfile:
+        x = 0
+        for line in txtfile:
+            dados[tipos[x]]  = line.rstrip('\n')            
+            x += 1 
+    print(dados)
+    response = requests.post(url,json=dados)
